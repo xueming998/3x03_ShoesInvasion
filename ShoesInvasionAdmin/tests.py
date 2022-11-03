11a65ea55cf4e44d7d1cce8d4442da87e7f33f6d
Kendrick Lee <65295251+kendricklq@users.noreply.github.com>
2022-11-03 17:57:36 +0800

    Complete Unit Testing Codes

--- ShoesInvasionAdmin/tests.py
+++ ShoesInvasionAdmin/tests.py
@@ -1,3 +1,103 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from ShoesInvasionApp.models.user import UserTable
 
-# Create your tests here.
+# HTTP Response Code Legend
+# HTTP_200_OK
+# HTTP_201_CREATED
+# HTTP_302_FOUND
+# HTTP_404_NOT_FOUND
+
+# Test if pages loads properly
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/ShoesInvasionAdmin/')
+        self.assertEqual(response.status_code, 200)
+
+# Admin Test Case
+class AdminTestCase(TestCase):
+    def setUp(self):
+        adminUser = UserTable.objects.create(
+            first_name = "Elon",
+            last_name = "Musk",
+            username = "elonmusk",
+            password = "teslatwitterez",
+            verify_password = "teslatwitterez",
+            email = "elonmusk@twitter.com",
+            phone = 87657987,
+            bannedStatus = 0,
+            verifiedStatus = 1,
+            verificationCode = "2i4othlsad",
+            lockedStatus = 0,
+            lockedCounter = 0,
+            accountType = "Admin",
+            unique_id = "1l3rjjr1212bju13rbjuo1br5j3q",
+            secret_key = ""
+        )
+        adminUser2 = UserTable.objects.create(
+            first_name = "Jack",
+            last_name = "Ma",
+            username = "jackma",
+            password = "alibababillionair",
+            verify_password = "alibababillionair",
+            email = "jackma@alibaba.com",
+            phone = 87657984,
+            bannedStatus = 0,
+            verifiedStatus = 1,
+            verificationCode = "2i4otasglsad",
+            lockedStatus = 0,
+            lockedCounter = 0,
+            accountType = "Admin",
+            unique_id = "iokwrsngio32klt4nho1iqnf",
+            secret_key = "NICN1K32512L3KN5"
+        )
+        user = UserTable.objects.create(
+            first_name = "Warren",
+            last_name = "Buffett",
+            username = "warrenbuffett",
+            password = "investingiseasy",
+            verify_password = "investingiseasy",
+            email = "buffett@berkshire.com",
+            phone = 87657184,
+            bannedStatus = 0,
+            verifiedStatus = 1,
+            verificationCode = "2ikq24nepi4otasglsad",
+            lockedStatus = 0,
+            lockedCounter = 0,
+            accountType = "User",
+            unique_id = "io1obr3jlkkwrsngio32klt4nho1iqnf",
+            secret_key = "NICN1K322KLTN1Q3512L3KN5"
+        )
+        adminUser.save()
+        adminUser2.save()
+        user.save()
+
+    def test_admin_can_get_details(self):
+        # New Product added as shown above
+        adminUsername = UserTable.objects.get(username = "elonmusk")
+        self.assertEqual(adminUsername.email, "elonmusk@twitter.com")
+        self.assertEqual(adminUsername.unique_id, "1l3rjjr1212bju13rbjuo1br5j3q")
+        
+        adminUsername2 = UserTable.objects.get(username = "jackma")
+        self.assertEqual(adminUsername2.email, "jackma@alibaba.com")
+        self.assertEqual(adminUsername2.unique_id, "iokwrsngio32klt4nho1iqnf")
+
+    def test_admin_login_without_otp(self):
+        c = Client()
+        response = c.post('/ShoesInvasionAdmin/login', {'username': 'elonmusk', 'password': 'teslatwitterez', "otpToken": ""})
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_login_with_otp(self):
+        c2 = Client()
+        response2 = c2.post('/ShoesInvasionAdmin/login', {'username': 'jackma', 'password': 'alibababillionair', "otpToken": "4546321"})
+        self.assertEqual(response2.status_code, 200)
+
+    def test_admin_ban_user(self):
+        c3 = Client()
+        userBanned = UserTable.objects.get(username = "warrenbuffett")
+        # bannedStatus = 0 means account not banned
+        self.assertFalse(userBanned.bannedStatus)
+        response3 = c3.post('/ShoesInvasionAdmin/ban_unban/', {'uid': 'io1obr3jlkkwrsngio32klt4nho1iqnf'})
+        self.assertEqual(response3.status_code, 200)
+        # bannedStatus = 1 means account banned
+        self.assertTrue(response3.content)
--- ShoesInvasionEditor/tests.py
+++ ShoesInvasionEditor/tests.py
@@ -1,3 +1,96 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from ShoesInvasionApp.models.user import UserTable
 
 # Create your tests here.
+
+# HTTP Response Code Legend
+# HTTP_200_OK
+# HTTP_201_CREATED
+# HTTP_302_FOUND
+# HTTP_404_NOT_FOUND
+
+# Test if pages loads properly
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/ShoesInvasionEditor/')
+        self.assertEqual(response.status_code, 200)
+
+# Test to create product and extract details from table
+class EditorTestCase(TestCase):
+    # Setting up the test case by creating a new product
+    def setUp(self):
+        editorUser = UserTable.objects.create(
+            first_name = "Jeff",
+            last_name = "Bezos",
+            username = "jeffbezos",
+            password = "amazonecommerce",
+            verify_password = "amazonecommerce",
+            email = "jeffbezos@amazon.com",
+            phone = 87657187,
+            bannedStatus = 0,
+            verifiedStatus = 1,
+            verificationCode = "2i4tpkl2",
+            lockedStatus = 0,
+            lockedCounter = 0,
+            accountType = "Editor",
+            unique_id = "3nklqyg3klern5y",
+            secret_key = "LC1J2K35LC1JK235")
+
+        editorUser2 = UserTable.objects.create(
+            first_name = "Bill",
+            last_name = "Gates",
+            username = "billgates",
+            password = "windows11ezmoney",
+            verify_password = "windows11ezmoney",
+            email = "billgates@microsoft.com",
+            phone = 89657187,
+            bannedStatus = 0,
+            verifiedStatus = 1,
+            verificationCode = "b1olrjn1o",
+            lockedStatus = 0,
+            lockedCounter = 0,
+            accountType = "Editor",
+            unique_id = "t43ionop3nr1p1onm31ft1")
+        editorUser.save()
+        editorUser2.save()
+
+    def test_admin_can_get_details(self):
+        # New Product added as shown above
+        editorUsername = UserTable.objects.get(username = "jeffbezos")
+        self.assertEqual(editorUsername.email, "jeffbezos@amazon.com")
+        self.assertEqual(editorUsername.unique_id, "3nklqyg3klern5y")
+        
+        adminUsername2 = UserTable.objects.get(username = "billgates")
+        self.assertEqual(adminUsername2.email, "billgates@microsoft.com")
+        self.assertEqual(adminUsername2.unique_id, "t43ionop3nr1p1onm31ft1")
+
+    def test_login_loads_properly(self):
+        response = self.client.get('http://127.0.0.1:8000/ShoesInvasionEditor/login')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='ShoesInvasionEditor/login.html')
+
+    def test_login(self):
+        c = Client()
+        # Dummy OTP because OTP is required to login but for unit test, unable to generate QR Code to do
+        response = c.post('/ShoesInvasionEditor/login', {'username': 'jeffbezos', 'password': 'amazonecommerce', "otpToken": "123456"})
+        self.assertEqual(response.status_code, 200)
+        c = Client()
+        # Dummy OTP because OTP is required to login but for unit test, unable to generate QR Code to do
+        response2 = c.post('/ShoesInvasionEditor/login', {'username': 'billgates', 'password': 'windows11ezmoney', "otpToken": ""})
+        self.assertEqual(response2.status_code, 200)
+
+    def test_create_product(self):
+        session = self.client.session
+        session['unique_id'] ='3nklqyg3klern5y'
+        session.save()
+        response = self.client.post('/ShoesInvasionEditor/create', 
+        {'product_name': 'Air Jordan 2 Retro SE',
+         'product_price': '355', 
+         "product_info": "Air Jordan 2 made its debut in 1987 as a sleeker, more streamlined version of its predecessor. This version of the AJ 2 features premium leather and an Air-Sole unit, making it the ultimate combination of performance and style.",
+         "product_brand": "Nike",
+         "status": "2",
+         "gender": "F",
+         "category": "Sneakers"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], 'manage')
